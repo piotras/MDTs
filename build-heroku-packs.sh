3b85ff3533fde29f347c026809f67480cd508078
piotras <piotrek.pokora@gmail.com>
2013-01-04 13:24:05 +0100

    Build php5-midgard2

--- build-heroku-packs.sh
+++ build-heroku-packs.sh
@@ -73,3 +73,27 @@ make
 make install
 
 # TODO Create tarball from built binaries and upload it to S3
+
+# Install php
+echo "Installing PHP"
+cd $HOME
+curl https://s3.amazonaws.com/php-lp/php-5.3.10.tar.gz | tar xz
+cd -
+
+# Build php5-midgard2 extension
+echo "Installing php5-midgard2"
+PHP5_MIDGARD2=midgard-php5-10.05.7
+PHP5_MIDGARD2_PKG=10.05.7.tar.gz
+curl -L -O https://github.com/midgardproject/midgard-php5/archive/$PHP5_MIDGARD2_PKG
+tar -xvf $PHP5_MIDGARD2_PKG
+cd $PHP5_MIDGARD2
+/app/php/bin/phpize
+export PKG_CONFIG_LIBDIR=/usr/lib/pkgconfig:$HOME/midgard2/lib/pkgconfig
+./configure --with-php-config=/app/php/bin/php-config
+make
+make install
+
+# WARNING! Keep in mind while creating buildpack. There's no way to check this directory automatically.
+# Installing shared extensions:     /app/php/lib/php/extensions/no-debug-non-zts-20090626/
+
+# TODO Create tarball from built binaries and upload it to S3
